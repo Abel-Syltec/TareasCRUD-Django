2019d9c55a669825c2f9df82a1f4c260c7ee60b0
Abel-Syltec <ablanco@syltec.com>
2025-02-27 14:41:47 +0100

    Modificaciones de las sesiones

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ah!j_4966$g6odp4kr=y8vvcbfo3r9saczgn(!ounlfzro03fl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'REDNER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
+#ESTABLECEMOS QUE LAS COOKIES DUREN 1 MINUTO Y QUE CUANDO SE CIERRE EL NAVEGADOR SE ELIMINEN
+SESSION_COOKIE_AGE = 10 #SON SEGUNDOS
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -129,3 +137,6 @@ LOGIN_URL = '/signin'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
--- tasks/views.py
+++ tasks/views.py
@@ -99,20 +99,23 @@ def exit(request):
     return redirect('home')
 
 def signin(request):
-    if request.method=='GET':
-        return render(request, 'signin.html',{
-        "form": AuthenticationForm
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            "form": AuthenticationForm
         })
     else:
-        user = authenticate(request, username = request.POST['username'], password = request.POST["password"])
+        user = authenticate(request, username=request.POST['username'], password=request.POST["password"])
         if user is None:
-            return render(request, 'signin.html',{
-            "form": AuthenticationForm,
-            "error": "Usuario o contraseñas incorrectos"
+            return render(request, 'signin.html', {
+                "form": AuthenticationForm,
+                "error": "Usuario o contraseñas incorrectos"
             })
         else:
             login(request, user)
-            return redirect('tasks')
+            response = redirect('tasks')
+            response.set_cookie('cookieUser',request.POST['username'] , max_age=10)
+            
+            return response
     
 @login_required
 def createTask(request):
